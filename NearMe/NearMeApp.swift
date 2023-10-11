5d63a43bc8abd9ef842485322a3da23173d51398
Yianni1980 <ioagian1980@gmail.com>
2023-10-11 12:40:51 +0300

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by ioannis on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
